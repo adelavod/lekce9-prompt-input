da34d5c66e2c9eac536da8a7a31820fa7aaff5d2
Adéla Krásová <>
2022-04-15 16:28:51 +0200

    [feature] na stránku vypíše, zda splňuje požadované parametry, v konzoli specifikuje, které nesplňuje

--- pracka.js
+++ pracka.js
@@ -3,12 +3,22 @@ let vyska = prompt("Zadej výšku");
 let sirka = prompt("Zadej šířku");
 let hloubka = prompt("Zadej hloubku");
 
-function porovnejPracky() {
-    if ((parseFloat(vyska) <= 70) && (parseFloat(sirka) <= 62) && (parseFloat(hloubka) <= 80)) {
-        document.getElementById('pracka').innerHTML ="Pračka splňuje rozměrové parametry.";
-        
-    } else {document.getElementById('pracka').innerHTML ="Pračka nesplňuje rozměrové parametry.";
+let idealniRozmery = [62, 70, 80];
 
-}};
+function porovnejPracky() {
+    if ((parseFloat(vyska) <= idealniRozmery[0]) && (parseFloat(sirka) <= idealniRozmery[1]) && (parseFloat(hloubka) <= idealniRozmery[2])) {
+        document.getElementById('pracka').innerHTML = "Pračka splňuje rozměrové parametry.";
+
+    } else {
+        document.getElementById('pracka').innerHTML = "Pračka nesplňuje rozměrové parametry.";
+        let zadaneRozmery = [vyska, sirka, hloubka];
+        for (let i in zadaneRozmery) {
+            if (zadaneRozmery[i] > idealniRozmery[i]) {
+
+                console.log("Rozměr " + zadaneRozmery[i] + " cm je příliš velký.")
+            }
+        }
+    }
+};
 
 porovnejPracky();
